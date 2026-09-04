4960c91a947cc577c286ba407f6502f6fd997a42
Karl Theodor Ruby <karl.ruby@tuta.com>
2026-09-04 11:04:36 +0200

    Add Refresh.ps1 script for quickly testing out new changes while developing

---
+++ Refresh.ps1
@@ -0,0 +1,17 @@
+# Stops the running AutoHotkey instance for this repo, then relaunches the master script.
+# Use this after editing a script to quickly reload it for testing.
+
+$RepoDir = $PSScriptRoot
+
+Get-CimInstance Win32_Process -Filter "Name = 'AutoHotkey64.exe'" |
+    Where-Object { $_.CommandLine -like "*$RepoDir*" } |
+    ForEach-Object {
+        Write-Host "Stopping: $($_.CommandLine)"
+        Stop-Process -Id $_.ProcessId -Force
+    }
+
+Start-Sleep -Milliseconds 300
+
+$MasterScript = Join-Path $RepoDir "WindowsShortcuts.ahk"
+Write-Host "Launching: $MasterScript"
+Start-Process -FilePath $MasterScript -WorkingDirectory $RepoDir
